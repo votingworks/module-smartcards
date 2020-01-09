339b0b188d3bed9151d0fd77ca3a71de0de5f1f1
Ben Adida <ben@adida.net>
2020-01-09 14:28:22 -0800

    tweak

--- writeVoterCardNoMockOverHTTP.py
+++ writeVoterCardNoMockOverHTTP.py
@@ -1,7 +1,7 @@
 
 import json, hashlib, time, http.client
 
-short_value = json.dumps({"t":"voter","bs":"12","pr":"23","c": round(time.time())})
+short_value = json.dumps({"t":"voter","bs":"2","pr":"6522","c": round(time.time())})
 
 client = http.client.HTTPConnection('localhost', 3001)
 client.request('POST', '/card/write-no-mock', short_value)
